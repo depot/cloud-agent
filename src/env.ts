0ce06708ac60f77a4893cae221c692a05cb4e541
Jacob Gillespie <jacobwgillespie@gmail.com>
2022-07-08 16:27:52 +0100

    Require environment variables

--- src/env.ts
+++ src/env.ts
@@ -3,6 +3,13 @@ import {config} from 'dotenv'
 config()
 
 export const CLOUD_AGENT_VERSION = process.env.CLOUD_AGENT_VERSION ?? 'devel'
-export const CLOUD_AGENT_CONNECTION_ID = process.env.CLOUD_AGENT_CONNECTION_ID ?? 'devel'
 export const DEPOT_API_ENDPOINT = process.env.DEPOT_API_ENDPOINT ?? 'https://depot.dev'
-export const DEPOT_API_TOKEN = process.env.DEPOT_API_TOKEN
+
+export const CLOUD_AGENT_CONNECTION_ID = requiredEnv('CLOUD_AGENT_CONNECTION_ID')
+export const DEPOT_API_TOKEN = requiredEnv('DEPOT_API_TOKEN')
+
+function requiredEnv(name: string): string {
+  const value = process.env[name]
+  if (!value) throw new Error(`Missing required environment variable ${name}`)
+  return value
+}
