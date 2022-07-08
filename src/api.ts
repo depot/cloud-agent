4f7927ad608e5a8ad2403b3067c526f81ae99941
Jacob Gillespie <jacobwgillespie@gmail.com>
2022-07-08 16:51:36 +0100

    Clean up env vars

--- src/api.ts
+++ src/api.ts
@@ -1,34 +1,27 @@
 import {request} from 'undici'
-import {CLOUD_AGENT_CONNECTION_ID, CLOUD_AGENT_VERSION, DEPOT_API_ENDPOINT, DEPOT_API_TOKEN} from './env'
+import {CLOUD_AGENT_API_ENDPOINT, CLOUD_AGENT_API_TOKEN, CLOUD_AGENT_CONNECTION_ID, CLOUD_AGENT_VERSION} from './env'
 
-const commonHeaders = {
-  Authorization: `Bearer ${DEPOT_API_TOKEN}`,
+const headers = {
+  Authorization: `Bearer ${CLOUD_AGENT_API_TOKEN}`,
   'User-Agent': `cloud-agent/${CLOUD_AGENT_VERSION}`,
   'Content-Type': 'application/json',
 }
 
+const id = CLOUD_AGENT_CONNECTION_ID
+
 export async function pingHealth() {
-  await request(`${DEPOT_API_ENDPOINT}/api/agents/cloud/${CLOUD_AGENT_CONNECTION_ID}/health`, {
-    method: 'POST',
-    headers: commonHeaders,
-  })
+  await request(`${CLOUD_AGENT_API_ENDPOINT}/${id}/health`, {method: 'POST', headers})
 }
 
 export async function getDesiredState() {
-  const res = await request(`${DEPOT_API_ENDPOINT}/api/agents/cloud/${CLOUD_AGENT_CONNECTION_ID}/desired-state`, {
-    method: 'POST',
-    headers: commonHeaders,
-  })
-  const body = await res.body.json()
-  return body
+  const res = await request(`${CLOUD_AGENT_API_ENDPOINT}/${id}/desired-state`, {method: 'POST', headers})
+  const data = await res.body.json()
+  return data
 }
 
 export async function reportCurrentState(state: any) {
-  const res = await request(`${DEPOT_API_ENDPOINT}/api/agents/cloud/${CLOUD_AGENT_CONNECTION_ID}/current-state`, {
-    method: 'POST',
-    headers: commonHeaders,
-    body: JSON.stringify(state),
-  })
-  const body = await res.body.json()
-  return body
+  const body = JSON.stringify(state)
+  const res = await request(`${CLOUD_AGENT_API_ENDPOINT}/${id}/current-state`, {method: 'POST', headers, body})
+  const data = await res.body.json()
+  return data
 }
--- src/env.ts
+++ src/env.ts
@@ -3,10 +3,10 @@ import {config} from 'dotenv'
 config()
 
 export const CLOUD_AGENT_VERSION = process.env.CLOUD_AGENT_VERSION ?? 'devel'
-export const DEPOT_API_ENDPOINT = process.env.DEPOT_API_ENDPOINT ?? 'https://depot.dev'
+export const CLOUD_AGENT_API_ENDPOINT = process.env.CLOUD_AGENT_API_ENDPOINT ?? 'https://depot.dev/api/agents/cloud'
 
 export const CLOUD_AGENT_CONNECTION_ID = requiredEnv('CLOUD_AGENT_CONNECTION_ID')
-export const DEPOT_API_TOKEN = requiredEnv('DEPOT_API_TOKEN')
+export const CLOUD_AGENT_API_TOKEN = requiredEnv('CLOUD_AGENT_API_TOKEN')
 
 function requiredEnv(name: string): string {
   const value = process.env[name]
