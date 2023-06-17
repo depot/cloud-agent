c1140cafc8a6d5e7564f6fe11f16fe7e143b83f6
Jacob Gillespie <jacobwgillespie@gmail.com>
2023-06-17 11:42:25 +0100

    Add authorization header to all requests

--- src/utils/grpc.ts
+++ src/utils/grpc.ts
@@ -1,9 +1,10 @@
 import {createPromiseClient, Interceptor} from '@bufbuild/connect'
 import {createConnectTransport} from '@bufbuild/connect-node'
 import {CloudService} from '../proto/depot/cloud/v2/cloud_connect'
-import {CLOUD_AGENT_API_URL, CLOUD_AGENT_VERSION} from './env'
+import {CLOUD_AGENT_API_URL, CLOUD_AGENT_CONNECTION_TOKEN, CLOUD_AGENT_VERSION} from './env'
 
-const userAgentInterceptor: Interceptor = (next) => async (req) => {
+const headerInterceptor: Interceptor = (next) => async (req) => {
+  req.header.set('Authorization', `Bearer ${CLOUD_AGENT_CONNECTION_TOKEN}`)
   req.header.set('User-Agent', `cloud-agent/${CLOUD_AGENT_VERSION}`)
   return await next(req)
 }
@@ -11,7 +12,7 @@ const userAgentInterceptor: Interceptor = (next) => async (req) => {
 const transport = createConnectTransport({
   httpVersion: '2',
   baseUrl: CLOUD_AGENT_API_URL,
-  interceptors: [userAgentInterceptor],
+  interceptors: [headerInterceptor],
 })
 
 export const client = createPromiseClient(CloudService, transport)
