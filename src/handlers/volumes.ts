ec61cc4747c4f1a88e34c07a5b2e2ee29dc0e57b
Chris Goller <goller@gmail.com>
2023-06-21 20:35:57 -0500

    feat(ceph): handle volume API
    Signed-off-by: Chris Goller <goller@gmail.com>

---
+++ src/handlers/volumes.ts
@@ -0,0 +1,171 @@
+import {
+  AuthorizeClientAction,
+  CreateClientAction,
+  CreateVolumeAction,
+  DeleteClientAction,
+  DeleteVolumeAction,
+  ResizeVolumeAction,
+} from '../proto/depot/cloud/v2/cloud_pb'
+import {
+  authCaps,
+  authGetJson,
+  authRm,
+  cephConfig,
+  createAuthEntity,
+  createBlockDevice,
+  createNamespace,
+  imageRm,
+  namespaceRm,
+  newClientName,
+  newImageSpec,
+  newOsdProfile,
+  newPoolSpec,
+} from '../utils/ceph'
+import {CLOUD_AGENT_CONNECTION_ID} from '../utils/env'
+import {reportError} from '../utils/errors'
+import {client} from '../utils/grpc'
+
+export async function startVolumeStream(signal: AbortSignal) {
+  while (!signal.aborted) {
+    try {
+      const stream = client.reconcileVolumes({connectionId: CLOUD_AGENT_CONNECTION_ID}, {signal})
+      for await (const response of stream) {
+        if (signal.aborted) return
+        switch (response.action.case) {
+          case 'createVolume':
+            await createVolume(response.action.value)
+            break
+          case 'resizeVolume':
+            await resizeVolume(response.action.value)
+            break
+          case 'deleteVolume':
+            await deleteVolume(response.action.value)
+            break
+          case 'createClient':
+            await createClient(response.action.value)
+            break
+          case 'authorizeClient':
+            await authorizeClient(response.action.value)
+            break
+          case 'deleteClient':
+            await deleteClient(response.action.value)
+            break
+        }
+      }
+    } catch (err: any) {
+      await reportError(err)
+    }
+  }
+}
+
+async function createVolume({volumeName, size}: CreateVolumeAction) {
+  const poolSpec = newPoolSpec(volumeName)
+  const imageSpec = newImageSpec(volumeName)
+
+  try {
+    await createNamespace(poolSpec)
+    await createBlockDevice(imageSpec, size)
+
+    await client.reportVolumeUpdates({
+      connectionId: CLOUD_AGENT_CONNECTION_ID,
+      update: {
+        case: 'createVolume',
+        value: {
+          volumeName,
+        },
+      },
+    })
+  } catch (err: any) {
+    await reportError(err)
+  }
+}
+
+async function resizeVolume(_action: ResizeVolumeAction) {
+  // TODO: resize volume
+}
+
+async function deleteVolume({volumeName}: DeleteVolumeAction) {
+  const imageSpec = newImageSpec(volumeName)
+  const poolSpec = newPoolSpec(volumeName)
+
+  try {
+    await imageRm(imageSpec)
+    await namespaceRm(poolSpec)
+
+    await client.reportVolumeUpdates({
+      connectionId: CLOUD_AGENT_CONNECTION_ID,
+      update: {
+        case: 'deleteVolume',
+        value: {
+          volumeName,
+        },
+      },
+    })
+  } catch (err: any) {
+    await reportError(err)
+  }
+}
+
+async function createClient({name}: CreateClientAction) {
+  const clientName = newClientName(name)
+  try {
+    await createAuthEntity(clientName)
+
+    await client.reportVolumeUpdates({
+      connectionId: CLOUD_AGENT_CONNECTION_ID,
+      update: {
+        case: 'createClient',
+        value: {
+          name,
+          clientName,
+        },
+      },
+    })
+  } catch (err: any) {
+    await reportError(err)
+  }
+}
+
+async function authorizeClient({volumeName, clientName}: AuthorizeClientAction) {
+  const osdProfile = newOsdProfile(volumeName)
+
+  try {
+    await authCaps(osdProfile, clientName)
+    const {key} = await authGetJson(clientName)
+    const config = await cephConfig()
+
+    await client.reportVolumeUpdates({
+      connectionId: CLOUD_AGENT_CONNECTION_ID,
+      update: {
+        case: 'authorizeClient',
+        value: {
+          clientName,
+          volumeName,
+          key,
+          config,
+          imageSpec: newImageSpec(volumeName),
+        },
+      },
+    })
+  } catch (err: any) {
+    await reportError(err)
+  }
+}
+
+async function deleteClient({clientName}: DeleteClientAction) {
+  try {
+    await authRm(clientName)
+
+    await client.reportVolumeUpdates({
+      connectionId: CLOUD_AGENT_CONNECTION_ID,
+      update: {
+        case: 'deleteClient',
+        value: {
+          clientName,
+        },
+      },
+    })
+  } catch (err: any) {
+    await reportError(err)
+  }
+}
--- src/utils/ceph.ts
+++ src/utils/ceph.ts
@@ -2,88 +2,75 @@ import {execa} from 'execa'
 
 const POOL = 'rbd'
 
-export function newImageSpec(volumeName: string): string {
+type PoolSpec = string
+type ImageSpec = string
+type ClientName = string
+type OsdProfile = string
+
+export function newPoolSpec(volumeName: string): PoolSpec {
+  return `${POOL}/${volumeName}`
+}
+
+export function newImageSpec(volumeName: string): ImageSpec {
   return `${POOL}/${volumeName}/${volumeName}`
 }
 
-export function newClientName(name: string): string {
+export function newClientName(name: string): ClientName {
   return `client.${name}`
 }
 
-export async function newVolume(volumeName: string, gigabytes: number): Promise<boolean> {
-  const imageSpec = newImageSpec(volumeName)
-  return (await createNamespace(POOL, volumeName)) && (await createBlockDevice(imageSpec, gigabytes))
-}
-
-export async function authorizeToVolume(volumeName: string, clientName: string): Promise<Auth | undefined> {
-  return (await authCaps(POOL, volumeName, clientName)) ? await authGetJson(clientName) : undefined
-}
-
-export async function deleteVolume(volumeName: string): Promise<boolean> {
-  const imageSpec = newImageSpec(volumeName)
-  return (await imageRm(imageSpec)) && (await namespaceRm(POOL, volumeName))
+export function newOsdProfile(volumeName: string): OsdProfile {
+  // Gives a user read-write access to the Ceph Block Devices in namespace.
+  return `'profile rbd pool=${POOL} namespace=${volumeName}'`
 }
 
 /*** Low-level Ceph functions ***/
 
-export async function listNamespaces(pool: string): Promise<string[]> {
-  interface Namespace {
-    name: string
+export async function createNamespace(poolSpec: PoolSpec) {
+  const {exitCode, stderr} = await execa('rbd', ['namespace', 'create', poolSpec])
+  // 17 is "already exists" a.k.a EEXIST.
+  if (exitCode == 0 || exitCode == 17) {
+    return
   }
 
-  const {exitCode, stdout} = await execa('rbd', ['namespace', 'ls', '--format', 'json', pool])
-  if (exitCode == 0) {
-    const namespaces = JSON.parse(stdout) as Namespace[]
-    return namespaces.map((namespace) => namespace.name)
-  }
-  return []
+  throw new Error(stderr)
 }
 
-export async function createNamespace(pool: string, namespace: string): Promise<boolean> {
-  try {
-    const {exitCode} = await execa('rbd', ['namespace', 'create', `${pool}/${namespace}`])
-    return exitCode == 0
-  } catch (e) {
-    console.log(e)
-    return false
+export async function createBlockDevice(imageSpec: ImageSpec, gigabytes: number) {
+  const {exitCode, stderr} = await execa('rbd', ['create', imageSpec, '--size', `${gigabytes}G`])
+  // 17 is "already exists" a.k.a EEXIST.
+  if (exitCode == 0 || exitCode == 17) {
+    return
   }
+
+  throw new Error(stderr)
 }
 
-export async function createBlockDevice(imageSpec: string, gigabytes: number): Promise<boolean> {
-  try {
-    const {exitCode} = await execa('rbd', ['create', imageSpec, '--size', `${gigabytes}G`])
-    return exitCode == 0
-  } catch (e) {
-    console.log(e)
-    return false
-  }
+export async function createAuthEntity(name: ClientName) {
+  const {exitCode, stderr} = await execa('ceph', ['auth', 'add', name])
+  // ceph auth add is idempotent.
+  if (exitCode == 0) return
+
+  throw new Error(stderr)
 }
 
-export async function createAuthEntity(name: string): Promise<boolean> {
-  try {
-    const {exitCode} = await execa('ceph', ['auth', 'add', name])
-    return exitCode == 0
-  } catch (e) {
-    console.log(e)
-    return false
-  }
-}
+export async function authCaps(osdProfile: OsdProfile, clientName: string) {
+  // We don't use get-or-create as the caps must always be the same.
+  const {exitCode, stderr} = await execa('ceph', [
+    'auth',
+    'caps',
+    clientName,
+    'mon',
+    // TODO: I'm not sure if it is better to have profile rbd here.
+    `'allow r'`,
+    'osd',
+    osdProfile,
+  ])
 
-export async function authCaps(pool: string, namespace: string, clientName: string): Promise<boolean> {
-  try {
-    const {exitCode} = await execa('ceph', [
-      'auth',
-      'caps',
-      clientName,
-      'osd',
-      // TODO: I don't think this is all that we need.
-      `'profile rbd pool=${pool} namespace=${namespace}'`,
-    ])
-    return exitCode == 0
-  } catch (e) {
-    console.log(e)
-    return false
-  }
+  // ceph auth caps is idempotent.
+  if (exitCode == 0) return
+
+  throw new Error(stderr)
 }
 
 export interface Auth {
@@ -91,60 +78,56 @@ export interface Auth {
   key: string
 }
 
-export async function authGetJson(clientName: string): Promise<Auth | undefined> {
-  try {
-    const {exitCode, stdout} = await execa('ceph', ['auth', 'get', clientName, '-f', 'json'])
-    if (exitCode == 0) {
-      // Parse the JSON output into an array of Auth objects
-      const auth = JSON.parse(stdout) as Auth[]
-      if (auth.length > 0) {
-        return auth[0]
-      }
+export async function authGetJson(clientName: ClientName): Promise<Auth> {
+  const {exitCode, stdout, stderr} = await execa('ceph', ['auth', 'get', clientName, '-f', 'json'])
+  if (exitCode == 0) {
+    // Parse the JSON output into an array of Auth objects
+    const auth = JSON.parse(stdout) as Auth[]
+    if (auth.length > 0) {
+      return auth[0]
     }
-  } catch (e) {
-    console.log(e)
+
+    throw new Error(`No auth found for ${clientName}`)
   }
+
+  throw new Error(stderr)
 }
 
-export async function authRm(name: string): Promise<boolean> {
-  try {
-    const {exitCode} = await execa('ceph', ['auth', 'rm', name])
-    return exitCode == 0
-  } catch (e) {
-    console.log(e)
-    return false
+export async function authRm(clientName: ClientName) {
+  const {exitCode, stderr} = await execa('ceph', ['auth', 'rm', clientName])
+  // ceph auth rm is idempotent.
+  if (exitCode == 0) {
+    return
   }
+
+  throw new Error(stderr)
 }
 
-export async function imageRm(imageSpec: string): Promise<boolean> {
-  try {
-    const {exitCode} = await execa('rbd', ['rm', imageSpec])
-    return exitCode == 0
-  } catch (e) {
-    console.log(e)
-    return false
+export async function imageRm(imageSpec: ImageSpec) {
+  const {exitCode, stderr} = await execa('rbd', ['rm', imageSpec])
+  // 2 is "image does not exist" a.k.a ENOENT.
+  if (exitCode == 0 || exitCode == 2) {
+    return
   }
+
+  throw new Error(stderr)
 }
 
-export async function namespaceRm(pool: string, namespace: string): Promise<boolean> {
-  try {
-    const {exitCode} = await execa('rbd', ['namespace', 'rm', `${pool}/${namespace}`])
-    return exitCode == 0
-  } catch (e) {
-    console.log(e)
-    return false
+export async function namespaceRm(poolSpec: PoolSpec) {
+  const {exitCode, stderr} = await execa('rbd', ['namespace', 'rm', poolSpec])
+  // 2 is "namespace does not exist" a.k.a ENOENT.
+  if (exitCode == 0 || exitCode == 2) {
+    return
   }
+
+  throw new Error(stderr)
 }
 
-export async function cephConfig(): Promise<string | undefined> {
-  try {
-    const {exitCode, stdout} = await execa('ceph', ['config', 'generate-minimal-conf'])
-    if (exitCode == 0) {
-      return stdout
-    }
-  } catch (e) {
-    console.log(e)
+export async function cephConfig(): Promise<string> {
+  const {exitCode, stdout, stderr} = await execa('ceph', ['config', 'generate-minimal-conf'])
+  if (exitCode == 0) {
+    return stdout
   }
-}
 
-// TODO: note somewhere that we don't use get-or-create as we don't know the caps at that time.
+  throw new Error(stderr)
+}
